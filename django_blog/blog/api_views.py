e27fea85276e50e7c97fea82664f0800ade2c441
Ghostlyjeff <odifirijide1234@gmail.com>
2025-04-03 22:52:04 +0100

    initialize all new changes from old deleted repo

---
+++ django_blog/blog/api_views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets, permissions
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# django_blog/django_blog/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users.views import register
+from blog.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('register/', register, name='register'),
 ]
