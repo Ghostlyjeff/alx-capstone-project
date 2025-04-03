ba7bade59d8ca097d63c6680a071647918ac4418
Ghostlyjeff <odifirijide1234@gmail.com>
2025-04-03 22:37:09 +0100

    initialize views in users and Blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
 
+def home(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
 # Create your views here.
